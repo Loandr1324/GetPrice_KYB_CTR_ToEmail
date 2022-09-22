085c5e92f6c993626f4a3519431b0f28ba999795
Loandr1234 <7034@balancedv.ru>
2022-09-23 00:59:10 +1000

    1. Создал предварительную структуру программы. 2. Прописал функцию чтения файла с остатками в DataFrame по FTP: get_stock_ftp. 3. Прописал функцию чтения файла с ценами из папки на сервере с помощью библиотеки smbclient: get_price. 4. Прописал функцию для основного кода программы: get_prices_to_email. 5. Начал прописывать функцию по подготовки/отправки полученных данных из DataFrame на почту. 5.1. Необходимо разделить данные полученного прайса на данные с ценами и без. 5.2. Реализовать отправку писем с данными с ценами на сайт и без цен на почту для дальнейшей отработки.

--- main.py
+++ main.py
@@ -1,16 +1,93 @@
-# This is a sample Python script.
+import io
+import config
+import pandas as pd
+from ftplib import FTP
+import smbclient
+import logging
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+logging.basicConfig(filename="KYB_CTR_to_abcp.log", level=logging.INFO)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_stock_ftp(file: str) -> pd.DataFrame:
+    """Получаем файл с остатками по FTP
+
+    :return DataFrame с остатками"""
+    host = config.FTP_AUTH['HOST']
+    user = config.FTP_AUTH['USER']
+    psw = config.FTP_AUTH['PSW']
+
+    with FTP(host) as ftp:
+        ftp.login(user=user, passwd=psw)
+        ftp.encoding = 'utf-8'
+        print(file)
+        r = io.BytesIO()
+        ftp.retrbinary('RETR ' + file, r.write, 1024)
+        df = pd.read_csv(io.BytesIO(r.getvalue()), delimiter=';')
+        df = df.drop(columns=['price', 'currency'])
+    return df
 
 
-# Press the green button in the gutter to run the script.
+def get_price():
+    """
+    Считываем файлы с ценами из папки в локальной сети
+
+    :return: DataFrame с ценами
+    """
+    # Получаем список файлов на сервере
+    path = config.LOCAL_PATH['PATH']
+    smbclient.ClientConfig(username=config.LOCAL_PATH['USER'], password=config.LOCAL_PATH['PSW'])
+    list_file = smbclient.listdir(path)
+
+    # Считываем все файлы с ценами KYB и CTR в DataFrame
+    df = pd.DataFrame()
+    for item in list_file:
+        if item.endswith('.xlsx') and (item.find('Цены KYB') != -1 or item.find('Цены CTR') != -1):
+            print(item) # TODO Удалить после тестов
+            path_file = path + "\\" + item
+            df_price = pd.read_excel(path_file)
+            df = pd.concat([df, df_price], axis=0, ignore_index=True)
+            df = df[['articul', 'brand', 'price']]
+    # df.to_excel('test.xlsx') # TODO  Удалить после тестов
+    return df
+
+
+def create_output_files(df):
+    print(df)
+    # print(df['price'].isna())
+    df_correct = df[~df['price'].isna()]
+    df_isnan = df[~df['price'].isna()]
+    print(df_correct)
+    pass
+
+
+def send_mail():
+    """ Отправляем файл на почту"""
+    pass
+
+
+def get_prices_to_email():
+    """ Объединяем файлы остатков по каждому городу и цен. Отправляем полученный файл на почту """
+    # Считываем прайсы
+    df_price = get_price()
+
+    # Подставляем цены для каждого склада и отправляем на почту
+    file_list_stock = config.FILE_LIST_STOCK
+    for file in file_list_stock:
+        df_stock = get_stock_ftp(file)
+        df_result = pd.merge(df_stock, df_price, on=['articul', 'brand'], how='left')
+        create_output_files(df_result)
+        df_result.to_excel('test1.xlsx')
+        break
+
+    send_mail()
+    pass
+
+
+def main():
+    get_prices_to_email()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
